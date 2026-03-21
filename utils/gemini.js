9c5cc6ba8215ae6a9e257d0b3e7b877b53d1dc74
TSAISHREEVARDHAN <tss.vardhan.official@gmail.com>
2026-03-21 22:30:44 +0530

    Update gemini.js

--- utils/gemini.js
+++ utils/gemini.js
@@ -1,7 +1,7 @@
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' });
+const model = genAI.getGenerativeModel({ model: 'gemini-2.5-flash-lite' });
 function cleanJSON(text) {
   return text.replace(/```json/gi, '').replace(/```/g, '').trim();
 }
